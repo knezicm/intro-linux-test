373546762acc6344abe02bc677ba3c79ed7c5cc6
Mladen Knezic <mladen.knezic@gmail.com>
2019-04-01 11:41:14 +0200

    removed files

--- zadatak-2/include/helloLib.h
+++
@@ -1,9 +0,0 @@
-#ifndef __HELLOLIB_H__
-#define __HELLOLIB_H__
-
-void printResult(int res);
-int addNumbers(int a, int b);
-void printFirstNumber(int num);
-void printSecondNumber(int num);
-
-#endif
